e859bfcbb230ddaacf98eb3c1137a8dd09ae349b
Azeythy <azeythy1@gmail.com>
2023-04-07 13:08:01 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// const dados= [id, nome, age, hobby={programar, estudar,jogar}];
+
+
+function clicar(user){
+    
+    // const User={}
+
+    user={id:1,nome:"",idade:1}
+
+    let id=Math.random(110).toFixed(2);
+    let nome=document.getElementById("name");
+    let age=document.getElementById("ages");
+
+    
+
+let td_id =document.getElementById("1").value
+let td_nome =document.getElementById("2").value
+let td_age =document.getElementById("3").value
+
+
+console.log(td_nome,td_age   )
+}
+clicar()
